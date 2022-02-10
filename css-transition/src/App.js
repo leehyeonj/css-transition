426fd97d3a2791fc67982372d3b0447f34a5ce12
leehyeonj <billaa@naver.com>
2022-02-10 18:07:19 +0900

    create react app

---
+++ css-transition/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import SpreadCard from './spread-card';
+
+function App() {
+  return (
+     <SpreadCard/>
+  );
+}
+
+export default App;
---
+++ css-transition/src/spread-card/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SpreadCard = () => {
+    return (
+        <div>
+            spread card
+        </div>
+    )
+}
+
+export default SpreadCard;
