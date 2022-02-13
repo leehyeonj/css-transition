3d537f0062f85824e6f78effffd4ed75846e892b
leehyeonj <billaa@naver.com>
2022-02-13 19:46:38 +0900

    카드 5장 펼치기 with classnames

--- css-transition/src/spread-card/index.js
+++ css-transition/src/spread-card/index.js
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import classnames from "classnames";
+import "./styles.css";
 
 const SpreadCard = () => {
-  return <div>hi</div>;
+  const [isSpread, setSpread] = useState(false);
+
+  const handleOnClick = () => {
+    setSpread(!isSpread);
+    console.log(isSpread);
+  };
+  return (
+    <div className={classnames("perspective")}>
+      <div className={classnames("card-container")}>
+        <button onClick={handleOnClick}>spread</button>
+        <div>
+          <div className={classnames(["card", "card1"], { spread1: isSpread })}>
+            A
+          </div>
+          <div className={classnames(["card", "card2"], { spread2: isSpread })}>
+            B
+          </div>
+          <div className={classnames(["card", "card3"], { spread3: isSpread })}>
+            C
+          </div>
+          <div className={classnames(["card", "card4"], { spread4: isSpread })}>
+            D
+          </div>
+          <div className={classnames(["card", "card5"], { spread5: isSpread })}>
+            e
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SpreadCard;
