113ec80047d873f96df4b1570850e621a2902e8c
leehyeonj <billaa@naver.com>
2022-02-16 00:39:15 +0900

    feat: card 컴포넌트 props로 바꾸기

--- css-transition/src/spread-card/index.js
+++ css-transition/src/spread-card/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import classnames from "classnames";
 import "./styles.css";
+import styled, { css } from "styled-components";
 
 const SpreadCard = () => {
   const [isSpread, setSpread] = useState(false);
@@ -9,30 +10,105 @@ const SpreadCard = () => {
     setSpread(!isSpread);
     console.log(isSpread);
   };
+
+  const cards = ["A", "B", "C", "D", "E", "A", "B", "C", "D", "E"];
+  const translate3dx = (index) => {
+    const value = -50 + 0.5 * index;
+    return value;
+  };
+  const translate3dy = (index) => {
+    const value = -10 - 0.5 * index;
+    return value;
+  };
+
+  const rotate = (index) => {
+    const value = -31 + 15 * index;
+    return value;
+  };
+
+  const rotate3dx = (index) => {
+    const value = -50 + 36 * index;
+    return value;
+  };
+
+  const rotate3dy = (index) => {
+    const value = -10 + 1.5 * index;
+    return value;
+  };
   return (
-    <div className={classnames("perspective")}>
-      <div className={classnames("card-container")}>
-        <button onClick={handleOnClick}>spread</button>
+    <Perspective onClick={handleOnClick}>
+      <CardContainer className={classnames("card-container")}>
         <div>
-          <div className={classnames(["card", "card1"], { spread1: isSpread })}>
-            A
-          </div>
-          <div className={classnames(["card", "card2"], { spread2: isSpread })}>
-            B
-          </div>
-          <div className={classnames(["card", "card3"], { spread3: isSpread })}>
-            C
-          </div>
-          <div className={classnames(["card", "card4"], { spread4: isSpread })}>
-            D
-          </div>
-          <div className={classnames(["card", "card5"], { spread5: isSpread })}>
-            e
-          </div>
+          {cards.map((p, index) => {
+            return (
+              <Card
+                isSpread={isSpread}
+                key={index}
+                translate3dx={translate3dx(index)}
+                translate3dy={translate3dy(index)}
+                rotate={rotate(index)}
+                rotate3dx={rotate3dx(index)}
+                rotate3dy={rotate3dy(index)}
+              >
+                {p}
+              </Card>
+            );
+          })}
         </div>
-      </div>
-    </div>
+      </CardContainer>
+    </Perspective>
   );
 };
 
+const Perspective = styled.div`
+  height: 100vh;
+  perspective: 2000px;
+  position: absolute;
+  width: 100vw;
+  background: #e7e7e7;
+`;
+
+const CardContainer = styled.div`
+  cursor: pointer;
+  left: 50%;
+  position: absolute;
+  top: 50%;
+  transform: rotateX(35deg) translate3d(-50%, -50%, 0);
+  height: 45vw;
+  width: 32vw;
+  max-height: 450px;
+  max-width: 320px;
+`;
+
+const Card = styled.div`
+  position: absolute;
+  background: #eeeae7;
+  width: 32vw;
+  height: 45vw;
+  max-height: 450px;
+  max-width: 320px;
+  min-height: 225px;
+  min-width: 160px;
+  border-radius: 20px;
+  box-shadow: 0 1px 2px 0 rgba(151, 150, 146, 0.4),
+    0 4px 12px 0 rgba(151, 150, 146, 0.4);
+  transition: all ease-in 0.3s;
+  user-select: none;
+  transform: rotate(-31deg)
+    translate3d(
+      ${(props) => props.translate3dx}%,
+      ${(props) => props.translate3dy}%,
+      0
+    );
+  ${(props) =>
+    props.isSpread &&
+    css`
+      transform: rotate(${(props) => props.rotate}deg)
+        translate3d(
+          ${(props) => props.rotate3dx}%,
+          ${(props) => props.rotate3dy}%,
+          0
+        );
+    `}
+`;
 export default SpreadCard;
