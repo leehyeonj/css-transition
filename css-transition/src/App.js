0ec7efb52586426fbad9b6c838bcf8f1bdc1f150
leehyeonj <billaa@naver.com>
2022-02-23 16:27:06 +0900

    new : flipping-card

--- css-transition/src/App.js
+++ css-transition/src/App.js
@@ -1,9 +1,11 @@
 import logo from "./logo.svg";
 import "./App.css";
 import SpreadCard from "./spread-card";
+import FlippingCard from "./flipping-card";
 
 function App() {
-  return <SpreadCard />;
+  // return <SpreadCard />;
+  return <FlippingCard />;
 }
 
 export default App;
