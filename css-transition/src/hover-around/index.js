a44e81e295d8629279c0116f6229e363e04dad83
leehyeonj <billaa@naver.com>
2022-02-27 20:48:24 +0900

    new : hover around card

---
+++ css-transition/src/hover-around/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+const HoverAround = () => {
+  return (
+    <div>
+      <div class="card">
+        <div class="card-content">
+          <h1>Just hover around</h1>
+          <p>
+            <small>
+              by{" "}
+              <a
+                href="https://www.fiverr.com/share/vLevr"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Ariona, Rian
+              </a>
+            </small>
+          </p>
+          <p class="related">
+            <strong>See also: </strong>
+            <a
+              href="https://codepen.io/ariona/details/LVZLGP/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Staged dropdown animation
+            </a>
+          </p>
+        </div>
+      </div>
+
+      <div class="iklan">
+        <p>Need PSD to HTML & CSS service?</p>
+        <a
+          href="https://www.fiverr.com/share/vLevr"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Start from <strong>$50</strong>
+        </a>
+      </div>
+    </div>
+  );
+};
+export default HoverAround;
